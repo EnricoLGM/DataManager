0fd2aa99a3cd4f541589406f8a60a8f228876635
EnricoLGM <enricolegumi44@gmail.com>
2022-01-20 18:11:56 +0100

    Popolazioe della classe GestioneBrani

--- app/src/main/java/com/example/datamanager/GestioneBrani.java
+++ app/src/main/java/com/example/datamanager/GestioneBrani.java
@@ -1,4 +1,15 @@
 package com.example.datamanager;
 
+import java.util.ArrayList;
+
 public class GestioneBrani {
+    ArrayList<Brano> lista=new ArrayList<>();
+
+    public GestioneBrani(ArrayList<Brano> lista) {
+        this.lista = lista;
+    }
+
+    public void addBrano(Brano b) {
+        lista.add(b);
+    }
 }
